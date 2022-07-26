4b0104f98fe80df8004acca6bf35b550af07bf8a
rafael_.moura <rafael.moura.dev.29@gmail.com>
2022-07-26 13:27:22 -0300

    ⚡️ Improve clone script

--- cloneScript.py
+++ cloneScript.py
@@ -2,28 +2,43 @@ import os
 import json
 import sys
 from dotenv import dotenv_values
-config ={**dotenv_values()}
+config = {**dotenv_values()}
 profile = config.get('PROFILE')
 region = config.get('REGION')
 database = 'database/repositories.json'
+ACCOUNT = config.get('ACCOUNT')
 
-if sys.argv.__len__() > 1:
-  path_delimiter = sys.argv[1]
+path_delimiter = os.path.sep
+
 
 def ls(path='.'):
-  array = []
-  ls =  os.listdir(path)
-  for entry in ls:
-    if (entry != 'ls.exe.stackdump'):
-      array.append(entry)
-  return array
+    array = []
+    ls = os.listdir(path)
+    for entry in ls:
+        if (entry != 'ls.exe.stackdump'):
+            array.append(entry)
+    return array
 
-def getFilePath(_file_path_, _ = "\\"):
-  pwd = os.path.realpath(os.getcwd())
 
-  return f'..{_}CodeCommit{_}{_file_path_}'
-def clone(repository, path,_ = "\\"):
-  os.system(f'git clone codecommit://{profile}@{repository} {path}{_}{repository}')
+def getFilePath(_file_path_, _=path_delimiter):
+    path = f'..{_}Accounts{_}{ACCOUNT}{_}CodeCommit'
+    file_path = f'{path}{_}{_file_path_}'
+    tenants = ls(path)
+    has = False
+    for t in tenants:
+        if t == _file_path_:
+            has = True
+
+    if not has:
+        os.mkdir(file_path)
+
+    return file_path
+
+
+def clone(repository, path, _=path_delimiter):
+    os.system(
+        f'git clone codecommit://{profile}@{repository} {path}{_}{repository}')
+
 
 def readDb():
     data = []
@@ -38,16 +53,16 @@ def readDb():
 repositories = readDb()
 
 for repository in repositories:
-  tenant = repository.get('tenant')
-  name = repository.get('repositoryName')
-  path = getFilePath(tenant, path_delimiter)
-  folders = ls(path)
-  x = 0
-  for folder in folders:
-    if name in folder:
-      x = 1
-  if not x:
-    print(f'[CLONE]: {tenant}/{name}')
-    clone(name,path,path_delimiter)
-  else:
-    print(f'[DISCARDED]: {tenant}/{name}')
+    tenant = repository.get('tenant')
+    name = repository.get('repositoryName')
+    path = getFilePath(tenant, path_delimiter)
+    folders = ls(path)
+    x = 0
+    for folder in folders:
+        if name in folder:
+            x = 1
+    if not x:
+        print(f'[CLONE]: {tenant}/{name}')
+        clone(name, path, path_delimiter)
+    else:
+        print(f'[DISCARDED]: {tenant}/{name}')
--- getAllRepositories.py
+++ getAllRepositories.py
@@ -1,7 +1,7 @@
 import os
 import json
 from dotenv import dotenv_values
-config ={**dotenv_values()}
+config = {**dotenv_values()}
 profile = config.get('PROFILE')
 region = config.get('REGION')
 
@@ -9,19 +9,30 @@ database = 'database/repositories.json'
 
 
 def listRepositories():
-  try:
-    return json.loads(os.popen(f'aws codecommit list-repositories --profile {profile} --region={region}').read())
-  except Exception as e:
-    print(e)
+    try:
+        return json.loads(os.popen(f'aws codecommit list-repositories --profile {profile} --region={region}').read())
+    except Exception as e:
+        print(e)
 
-def writeRepositories(objects, db):
-    json_object = json.dumps(objects, indent=4)
 
-    if os.path.exists(db):
-        with open(db, 'w', encoding='utf8') as f:
-            f.write(json_object)
-    else:
-        raise Exception('Arquivo não existe!')
+def writeRepositories(objects, db, retry=2, isJson=True):
+    try:
+        wObjetcs = objects
+        if isJson:
+
+            wObjetcs = json.dumps(objects, indent=4)
+
+        if os.path.exists(db):
+            with open(db, 'w', encoding='utf8') as f:
+                f.write(wObjetcs)
+        else:
+            os.system(f'touch {db}')
+            if retry > 0:
+                writeRepositories(objects, db, retry - 1)
+            else:
+                print('Arquivo não existe!')
+    except Exception as e:
+        print('Não foi possível escrever!')
 
 
 def get_repository(_repository):
@@ -43,6 +54,7 @@ def get_repository_tags(_repository):
     resource = list_tags_for_resource(arn)
     return resource['tags']
 
+
 repositories = listRepositories().get('repositories')
 
 core = []
@@ -50,24 +62,22 @@ btg_layer = []
 outros = []
 
 for repository in repositories:
-  name = repository['repositoryName']
-  tags = get_repository_tags(name)
-  if 'boilerplate' in name:
-    repository['tenant'] = 'boilerplate'
-  elif 'tenant' in tags:
-    repository['tenant'] = tags['tenant']
-  else:
-    repository['tenant'] = 0
-  
-  print(f'[{repository["tenant"]}]: {name}')
+    name = repository['repositoryName']
+    tags = get_repository_tags(name)
+    if 'boilerplate' in name:
+        repository['tenant'] = 'boilerplate'
+    elif 'tenant' in tags:
+        repository['tenant'] = tags['tenant']
+    else:
+        repository['tenant'] = 0
 
+    print(f'[{repository["tenant"]}]: {name}')
 
 
 writeRepositories(repositories, database)
 # writeRepositories(core, 'core.json')
 # writeRepositories(btg_layer, 'btg-layer.json')
 # writeRepositories(outros, 'outros.json')
-
 
 
 print(repositories)
